a00fc2f5205061a34cd23a12a977a97bf82ed6dd
VasiliyLik <vasya_li@mail.ru>
2023-07-08 21:58:38 +0300

    Решение задачи

---
+++ src/ProductSet.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public enum ProductSet {
+    BREAD,
+    SUGAR,
+    VODKA,
+    MILK,
+    SALO,
+    SOLT,
+    EGGS,
+    LIMON,
+    WISKEY;
+
+    private static final Random random = new Random();
+
+    //метод для рандомного выбора Продукта из предложенного списка
+    public static ProductSet randomSet() {
+        ProductSet[] directions = values();
+        return directions[random.nextInt(directions.length)];
+    }
+}
